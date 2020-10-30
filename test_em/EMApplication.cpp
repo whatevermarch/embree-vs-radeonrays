c4367a9be3cecda85d996923003cf3cdcd804883
Krittin Meenrattanakorn <whatevermarch@gmail.com>
2020-10-30 01:43:11 +0100

    Change to tile rendering paradigm, add OpenMP support

--- test_em/EMApplication.cpp
+++ test_em/EMApplication.cpp
@@ -1,7 +1,14 @@
 #include <chrono>
 #include <iostream>
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 #include "test_em/EMApplication.h"
+
+#define TILE_SIZE 8
+
 
 using namespace std::chrono;
 
@@ -71,37 +78,94 @@ void EMApplication::Run(uint32_t res, MeshData* mesh_data, std::vector<uint32_t>
     //  construct rays
     using RayHit = RTCRayHit;
     std::vector<RayHit> queries(res * res);
-    for (int x = 0; x < res; ++x)
-	{
-		for (int y = 0; y < res; ++y)
-		{
-			auto i = res * y + x;
+    
+#ifdef _OPENMP
+    //  subdivide all rays into many tiles
+    int num_space_x = (res + TILE_SIZE - 1) / TILE_SIZE;
+    int num_space_y = (res + TILE_SIZE - 1) / TILE_SIZE;
+    int num_tiles = num_space_x * num_space_y;
+    std::vector<int> num_rays_per_tile(num_tiles);
 
-			queries[i].ray.org_x = 0.f;
+    #pragma omp parallel for schedule(guided, 1)
+    for (int tile_idx = 0; tile_idx < num_tiles; tile_idx++)
+    {
+        int init_x = (tile_idx % num_space_x) * TILE_SIZE;
+        int init_y = (tile_idx / num_space_x) * TILE_SIZE;
+
+        //  determine the number of rays in this tile
+        int num_rays_x = (res - init_x < TILE_SIZE) ? (res - init_x) : TILE_SIZE,
+            num_rays_y = (res - init_y < TILE_SIZE) ? (res - init_y) : TILE_SIZE;
+        num_rays_per_tile[tile_idx] = num_rays_x * num_rays_y;
+
+        auto ray_itr = queries.begin() + (TILE_SIZE * TILE_SIZE) * tile_idx;
+
+        for (int y = init_y; y < init_y + num_rays_y; y++)
+        {
+            for (int x = init_x; x < init_x + num_rays_x; x++)
+            {
+                RayHit& ray = *(ray_itr++);
+                ray.ray.org_x = 0.f;
+                ray.ray.org_y = 250.f;
+                ray.ray.org_z = 0.f;
+
+                ray.ray.dir_x = -1.f;
+                ray.ray.dir_y = -1.f + (2.f / res) * y;
+                ray.ray.dir_z = -1.f + (2.f / res) * x;
+
+                ray.ray.tnear = 0.001f;
+                ray.ray.tfar = 100000.f;
+
+                ray.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+                ray.hit.primID = RTC_INVALID_GEOMETRY_ID;
+            }
+        }
+    }
+#else
+    for (int y = 0; y < res; ++y)
+    {
+        for (int x = 0; x < res; ++x)
+        {
+            auto i = res * y + x;
+
+            queries[i].ray.org_x = 0.f;
             queries[i].ray.org_y = 250.f;
-			queries[i].ray.org_z = 0.f;
+            queries[i].ray.org_z = 0.f;
 
-			queries[i].ray.dir_x = -1.f;
-			queries[i].ray.dir_y = -1.f + (2.f / res) * y;
-			queries[i].ray.dir_z = -1.f + (2.f / res) * x;
+            queries[i].ray.dir_x = -1.f;
+            queries[i].ray.dir_y = -1.f + (2.f / res) * y;
+            queries[i].ray.dir_z = -1.f + (2.f / res) * x;
 
-			queries[i].ray.tnear = 0.001f;
-			queries[i].ray.tfar = 100000.f;
-            queries[i].ray.time = 0.f;
+            queries[i].ray.tnear = 0.001f;
+            queries[i].ray.tfar = 100000.f;
 
             queries[i].hit.geomID = RTC_INVALID_GEOMETRY_ID;
             queries[i].hit.primID = RTC_INVALID_GEOMETRY_ID;
-		}
-	}
+        }
+    }
+#endif
 
-    //  trace ray streams
-    //  ToDo : activate openmp and subdivide works across threads
+    std::cout << "construct rays success!" << std::endl;
+
+    //  trace ray streams (warm up)
     rtcIntersect1M(scene, &context, queries.data(), res * res, sizeof(RayHit));
 
+    std::cout << "start timing..." << std::endl;
     //  measure time
     auto time_start = high_resolution_clock::now();
+
+#ifdef _OPENMP
+    #pragma omp parallel for schedule(guided, 1)
+    for (int tile_idx = 0; tile_idx < num_tiles; tile_idx ++)
+    {
+        auto ray_itr = queries.begin() + (TILE_SIZE * TILE_SIZE) * tile_idx;
+
+        rtcIntersect1M(scene, &context, &(*ray_itr), num_rays_per_tile[tile_idx], sizeof(RayHit));
+    }
+#else
     for (int i = 0; i < 10; i++)
         rtcIntersect1M(scene, &context, queries.data(), res * res, sizeof(RayHit));
+#endif
+
     auto time_end = high_resolution_clock::now();
 
     duration<double, std::milli> delta_time = time_end - time_start;
@@ -109,6 +173,35 @@ void EMApplication::Run(uint32_t res, MeshData* mesh_data, std::vector<uint32_t>
     std::cout << "Trace time: " << delta_time.count() << "ms\n";
 	std::cout << "Throughput: " << (res * res) / (delta_time.count() * 1e-3) * 1e-6 * 10 << " MRays/s\n";
 
+#ifdef _OPENMP
+    //  gather result
+    for (int y = 0; y < res; ++y)
+    {
+        for (int x = 0; x < res; ++x)
+        {
+            int wi = res * (res - 1 - y) + x;
+
+            int tile_idx_x = x / TILE_SIZE,
+                tile_idx_y = y / TILE_SIZE;
+            int tile_idx = tile_idx_y * num_space_x + tile_idx_x;
+            int pix_idx_x = x % TILE_SIZE,
+                pix_idx_y = y % TILE_SIZE;
+            int num_pix_x = (res - x < TILE_SIZE) ? (res - x) : TILE_SIZE;
+            int pix_idx = pix_idx_y * num_pix_x + pix_idx_x;
+            int i = (TILE_SIZE * TILE_SIZE) * tile_idx + pix_idx;
+
+            if (queries[i].hit.geomID != RTC_INVALID_GEOMETRY_ID)
+            {
+                out_data[wi] = 0xff000000 | (uint32_t(queries[i].hit.u * 255) << 8) |
+                    (uint32_t(queries[i].hit.v * 255) << 16);
+            }
+            else
+            {
+                out_data[wi] = 0xff101010;
+            }
+        }
+    }
+#else
     //  gather result
 	for (int y = 0; y < res; ++y)
 	{
@@ -128,6 +221,7 @@ void EMApplication::Run(uint32_t res, MeshData* mesh_data, std::vector<uint32_t>
 			}
 		}
 	}
+#endif
 
     rtcReleaseScene(scene);
 }
